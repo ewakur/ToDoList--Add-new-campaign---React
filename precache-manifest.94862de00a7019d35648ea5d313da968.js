7c16c8ddfdc03efc0571ebda3121662703d8c7a0
ewakur <ewa.kurczab13@gmail.com>
2020-10-23 11:28:53 +0200

    Updates

---
+++ precache-manifest.94862de00a7019d35648ea5d313da968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1292ac4e56add4e2315c3017e50bc4f8",
+    "url": "/ToDoList--Add-new-campaign---React/index.html"
+  },
+  {
+    "revision": "3e774c49c69135e0673b",
+    "url": "/ToDoList--Add-new-campaign---React/static/css/main.212fef74.chunk.css"
+  },
+  {
+    "revision": "2133b29e1e3a7156dae0",
+    "url": "/ToDoList--Add-new-campaign---React/static/js/2.1140daa3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList--Add-new-campaign---React/static/js/2.1140daa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e774c49c69135e0673b",
+    "url": "/ToDoList--Add-new-campaign---React/static/js/main.a20ccf65.chunk.js"
+  },
+  {
+    "revision": "48ea82a4994fc21f11c8",
+    "url": "/ToDoList--Add-new-campaign---React/static/js/runtime-main.bf0cd5e6.js"
+  }
+]);
